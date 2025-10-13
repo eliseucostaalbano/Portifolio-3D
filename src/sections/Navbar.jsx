399c785a127f095d80d4568a7c3f75d746204d19
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-10-13 14:23:09 -0300

    criando o comonete navbar  e trazendo mais assets

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+
 
 const Navbar = () => {
   return (
-    <div className='text-white'>Navbar</div>
+    <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
+        <div className='max-w-7xl mx-auto'>
+         <div className='flex items-center justify-between py-5 mx-auto c-space'>
+            <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white'>Eliseu</a>
+            <button>
+              <img src={"assets/menu.svg"} alt="menu" className='w-6 h-6'/>
+            </button>
+       </div>   
+        </div>
+    </header>
   )
 }
 
