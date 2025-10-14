87a0c79c323d8e91ccce9305041a20a21a27572d
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-10-14 13:47:59 -0300

    concluido a navBar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,18 +1,59 @@
+import { useState } from "react";
+import { navLinks } from "../constants/index.js";
 
-
-const Navbar = () => {
-  return (
-    <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
-        <div className='max-w-7xl mx-auto'>
-         <div className='flex items-center justify-between py-5 mx-auto c-space'>
-            <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white'>Eliseu</a>
-            <button>
-              <img src={"assets/menu.svg"} alt="menu" className='w-6 h-6'/>
-            </button>
-       </div>   
-        </div>
-    </header>
+const NavItems =() => {
+  return(
+    <ul className="nav-ul">
+      {navLinks.map(item => (
+        <li key={item.id} className="nav-li">
+          <a href={item.href} className="nav-li_a">
+            {item.nome}
+          </a>
+        </li>
+      ))}
+    </ul>
   )
 }
 
-export default Navbar
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex items-center justify-between py-5 mx-auto c-space">
+          <a
+            href="/"
+            className="text-neutral-400 font-bold text-xl hover:text-white"
+          >
+            Eliseu
+          </a>
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none flex sm:hidden"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="menu"
+              className="w-6 h-6"
+            />
+          </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
+        </div>
+      </div>
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems onClick={closeMenu} />
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
