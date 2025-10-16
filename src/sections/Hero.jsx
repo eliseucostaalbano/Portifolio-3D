9c7a18020986c678a475660f5ca1033d49c0e019
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-10-16 14:32:48 -0300

    botando um objeto 3D

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,39 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import { useMediaQuery } from "react-responsive"
+import SalaHacker from "../components/SalaHacker"
+import Loader from "../components/Canvas"
+import { calcularTamanhos } from "../constants/index"
+import { Leva } from 'leva';
 
 
 const Hero = () => {
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const tamanhos = calcularTamanhos(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
          Olá eu sou Eliseu <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Sou um Desenvolvedor Full-stack e de Jogos</p>
+        <p className="hero_tag text-gray_gradient">Trabalho na area de Desenvolvimento</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+       <Canvas className="w-full h-full ">
+        <Suspense fallback={<Loader />}>
+           <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+           <SalaHacker scale={tamanhos.deskScale} position={tamanhos.deskPosition} rotation={[0, -Math.PI, 0]} />
+            <Leva hidden />
+           <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+        </Suspense>
+        </Canvas> 
       </div>
     </section>
   )
