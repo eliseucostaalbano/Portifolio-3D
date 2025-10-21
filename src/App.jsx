be17ff78bc853f74e1c70e6039cce0681ca9893f
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-10-21 13:14:41 -0300

    criando o restante dos componetes basicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Navbar from './sections/Navbar.jsx'
 import Hero from './sections/Hero.jsx'
+import Sobre from './sections/Sobre.jsx'
+import Projetos from './sections/Projetos.jsx'
+import Clientes from './sections/Clientes.jsx'
+import Experiencias from './sections/Experiencias.jsx'
+import Contato from './sections/Contato.jsx'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar />
       <Hero />
+      <Sobre />
+      <Projetos />
+      <Clientes />
+      <Experiencias />
+      <Contato />
+      
       </main>
   )
 }
